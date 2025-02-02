92a7658f8423ece3c7add4ee5da4c603b069d88c
LAGonauta <lagonauta@gmail.com>
2025-02-01 22:20:01 -0500

    SoundAPI: OpenAL support

--- src/gui/wxgui/GeneralSettings2.cpp
+++ src/gui/wxgui/GeneralSettings2.cpp
@@ -55,6 +55,7 @@ const wxString kDirectSound("DirectSound");
 const wxString kXAudio27("XAudio2.7");
 const wxString kXAudio2("XAudio2");
 const wxString kCubeb("Cubeb");
+const wxString kOpenAL("OpenAL");
 
 const wxString kPropertyPersistentId("PersistentId");
 const wxString kPropertyMiiName("MiiName");
@@ -423,6 +424,8 @@ wxPanel* GeneralSettings2::AddAudioPage(wxNotebook* notebook)
 			m_audio_api->Append(kXAudio2);
 		if (IAudioAPI::IsAudioAPIAvailable(IAudioAPI::Cubeb))
 			m_audio_api->Append(kCubeb);
+		if (IAudioAPI::IsAudioAPIAvailable(IAudioAPI::OpenAL))
+			m_audio_api->Append(kOpenAL);
 
 		m_audio_api->SetSelection(0);
 		m_audio_api->SetToolTip(_("Select one of the available audio back ends"));
@@ -1038,6 +1041,8 @@ void GeneralSettings2::StoreConfig()
 		config.audio_api = IAudioAPI::XAudio2;
 	else if (m_audio_api->GetStringSelection() == kCubeb)
 		config.audio_api = IAudioAPI::Cubeb;
+	else if (m_audio_api->GetStringSelection() == kOpenAL)
+		config.audio_api = IAudioAPI::OpenAL;
 
 	config.audio_delay = m_audio_latency->GetValue();
 	config.tv_channels = (AudioChannels)m_tv_channels->GetSelection();
@@ -1729,6 +1734,8 @@ void GeneralSettings2::ApplyConfig()
 		m_audio_api->SetStringSelection(kXAudio2);
 	else if(config.audio_api == IAudioAPI::Cubeb)
 		m_audio_api->SetStringSelection(kCubeb);
+	else if (config.audio_api == IAudioAPI::OpenAL)
+		m_audio_api->SetStringSelection(kOpenAL);
 
 	SendSliderEvent(m_audio_latency, config.audio_delay);
 
@@ -1836,6 +1843,8 @@ void GeneralSettings2::OnAudioAPISelected(wxCommandEvent& event)
 		api = IAudioAPI::XAudio2;
 	else if (m_audio_api->GetStringSelection() == kCubeb)
 		api = IAudioAPI::Cubeb;
+	else if (m_audio_api->GetStringSelection() == kOpenAL)
+		api = IAudioAPI::OpenAL;
 	else
 	{
 		wxFAIL_MSG("invalid audio api selected!");
